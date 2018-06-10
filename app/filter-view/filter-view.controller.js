3670fee717a868b9c29a223e874292e795c5b584
mzayed <maha.zayed@zoho.com>
2018-06-10 18:37:48 -0400

    Added filtering logic for search results. Some minor UI tweaking left

--- app/filter-view/filter-view.controller.js
+++ app/filter-view/filter-view.controller.js
@@ -4,10 +4,18 @@ angular.module('filterView', [])
     .controller('filterViewCtrl', ['dataService', '$scope', function(dataService, $scope) {
 
 
+        $scope.showLoading = true;
+        $scope.showResults = false;
+
         // make the api call to get the list of results
         dataService.getSearchResults().then(function(response) {
             console.log("what do we have as our response ", response);
+            $scope.users = response.results;
             $scope.resultCt = response.info.results;
+            if ($scope.users.length > 0){
+                $scope.showLoading = false;
+                $scope.showResults = true;
+            }
         });
 
     }]);
