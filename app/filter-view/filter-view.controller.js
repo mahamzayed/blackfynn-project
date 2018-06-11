02a7bbee887bd2c0a7852af653f81c7f4dc18a23
mzayed <maha.zayed@zoho.com>
2018-06-11 12:17:15 -0400

    Made final touches/fixes to project'

--- app/filter-view/filter-view.controller.js
+++ app/filter-view/filter-view.controller.js
@@ -7,18 +7,19 @@ angular.module('filterView', [])
         $scope.showLoading = true;
         $scope.showResults = false;
         $scope.noResults = false;
-        $scope.resultsText = "";
 
         // make the api call to get the list of results
         dataService.getSearchResults().then(function(response) {
-            console.log("what do we have as our response ", response);
             $scope.users = response.results;
             $scope.resultCt = response.info.results;
             if ($scope.users.length > 0){
                 $scope.showLoading = false;
                 $scope.showResults = true;
-                $scope.resultsText = "Results";
             }
         });
+
+        $scope.filterName = function(user){
+
+        }
 
     }]);
